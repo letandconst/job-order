665f467aa7089757d28097b7b44fd5892c974784
Jm <jmdelasalas.dev@gmail.com>
2023-08-27 14:40:12 +0800

    Fixed amount in labor column. Added decimal places

--- client/src/pages/JobOrder/modules/TabularData.tsx
+++ client/src/pages/JobOrder/modules/TabularData.tsx
@@ -5,8 +5,8 @@ import { useData } from '../../../context/DataContext';
 interface TabularDataProps {
 	tableHeader: Array<string>;
 	text: string;
-	total: number;
-	data: Array<any>;
+	total: number | undefined;
+	data: Array<any> | undefined;
 }
 const TabularData = ({ tableHeader, data, total, text }: TabularDataProps) => {
 	const isTwoItems = tableHeader.length === 2;
@@ -14,25 +14,41 @@ const TabularData = ({ tableHeader, data, total, text }: TabularDataProps) => {
 	const { products } = useData();
 
 	return (
-		<Table variant='simple'>
+		<Table
+			variant='simple'
+			mt='32px'
+		>
 			<Thead border='1px solid #e2e8f0'>
 				<Tr>
-					{isTwoItems ? <Th borderRight='1px solid #e2e8f0'></Th> : <Th borderRight='1px solid #e2e8f0'>{tableHeader[0]}</Th>}
+					{isTwoItems ? (
+						<Th
+							borderRight='1px solid #e2e8f0'
+							w='25%'
+						></Th>
+					) : (
+						<Th borderRight='1px solid #e2e8f0'>{tableHeader[0]}</Th>
+					)}
 					<Th borderRight='1px solid #e2e8f0'>{tableHeader[isTwoItems ? 0 : 1]}</Th>
 					<Th borderRight='1px solid #e2e8f0'>{tableHeader[isTwoItems ? 1 : 2]}</Th>
 				</Tr>
 			</Thead>
 			<Tbody border='1px solid #e2e8f0'>
 				{Array.from({ length: 15 }).map((_, rowIndex) => {
-					const item = data[rowIndex] || {};
+					const item = data?.[rowIndex] || {};
 					const findProduct = products.find((product) => product._id === item.ProductID);
 					const productPrice = findProduct?.price;
+
+					let totalLabor;
+
+					if (typeof item?.labor !== 'undefined' && item.labor) {
+						totalLabor = item.labor.toFixed(2);
+					}
 
 					return (
 						<Tr key={rowIndex}>
 							{isTwoItems ? <Td borderRight='1px solid #e2e8f0'></Td> : <Td borderRight='1px solid #e2e8f0'>{item.Quantity}</Td>}
 							<Td borderRight='1px solid #e2e8f0'>{isTwoItems ? item.request : findProduct?.name}</Td>
-							<Td>{isTwoItems ? item.labor : productPrice?.toFixed(2)}</Td>
+							<Td textAlign='right'>{isTwoItems ? totalLabor : productPrice?.toFixed(2)}</Td>
 						</Tr>
 					);
 				})}
@@ -42,7 +58,12 @@ const TabularData = ({ tableHeader, data, total, text }: TabularDataProps) => {
 						<Box fontWeight='700'>{text}</Box>
 					</Td>
 					<Td>
-						<Box fontWeight='700'>{total.toFixed(2)}</Box>
+						<Box
+							fontWeight='700'
+							textAlign='right'
+						>
+							{total?.toFixed(2)}
+						</Box>
 					</Td>
 				</Tr>
 			</Tbody>
